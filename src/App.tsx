cecf729fe81dcc5c1ae997d1418b30445272dfda
Michael Benton-Sanchez <mbs652001@gmail.com>
2026-02-25 02:47:05 -0500

    fixed more import errors"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Staff from "./pages/Staff";
+// import Footer from "./components/Footer";
+// import Staff from "./pages/Staff";
 import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Contact from "./pages/Contact";
+// import Contact from "./pages/Contact";
 import MainSite from "./pages/MainSite";
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<MainSite />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/staff" element={<Staff />} />
+        {/* <Route path="/contact" element={<Contact />} />
+        <Route path="/staff" element={<Staff />} /> */}
       </Routes>
       {/* <Footer></Footer> */}
     </>
